6d958e3f636a17f80824cb25ac0f6a1e0ffa2504
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-21 00:18:25 +0600

    visited button click color change

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -5,18 +5,18 @@ const Country = ({country}) => {
   const {name,flags,population,area,cca3} = country;
   const [visited, setVisited] = useState(false)
   const handleVisited = () => {
-    setVisited(true)
+    setVisited(!visited)
   }
   return (
-    <div className='country'>
+    <div className={`country ${visited && 'visited'}`}>
       <h3>{name.official} </h3>
       <img src={flags.png} alt="" />
       <p>Common Name: {name.common}</p>
       <p>Population: {population} </p>
       <p>Area: {area} </p>
       <p>Code: {cca3}</p>
-      <button onClick={handleVisited}>Visited</button>
-      {visited && 'I have visited the country'}
+      <button onClick={handleVisited}>{visited?'Visited': 'Going'}</button>
+      {visited ? 'I have visited the country' : 'I want to visit'}
     </div>
   );
 };
